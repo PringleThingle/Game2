21d36c1c3a924413863c7cc2e329e8b95090f2dd
Duncan Pringle <pringlethingle@gmail.com>
2025-09-01 17:08:48 +0100

    Menu now pops up when you press escape, disappears if you hover over the "X" however need to work out better logic for if you've clicked on a menu object. Game also pauses when you open the menu

--- Game2/main.cpp
+++ Game2/main.cpp
@@ -14,6 +14,64 @@ struct Planet
     sf::Color color = sf::Color(rand() % 256, rand() % 256, rand() % 256);
 };
 
+//Function that divides a vector by a scalars
+sf::Vector2f divideVectorByDouble(sf::Vector2f v1, double scalar) {
+
+    return sf::Vector2f(v1.x / scalar, v1.y / scalar);
+}
+
+
+struct Menu {
+
+    sf::RectangleShape backGround;
+    sf::RectangleShape closeMenuButton;
+    sf::Font menuFont;
+    float windowLength;
+    float windowHeight;
+
+public:
+    Menu() 
+    {
+        //---------------------BackGround-------------------------------
+        backGround.setFillColor({ 77, 80, 84 });
+        backGround.setOutlineThickness(5);
+        backGround.setOutlineColor({ 16, 89, 201 });
+        //---------------------------------------------------------------
+
+        //---------------------BackGround-------------------------------
+        closeMenuButton.setFillColor({ 181, 20, 44 });
+        closeMenuButton.setOutlineThickness(1);
+        closeMenuButton.setOutlineColor({ 150, 89, 84 });
+        //---------------------------------------------------------------
+
+
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(backGround);
+        window.draw(closeMenuButton);
+    }
+
+    void updateLayout(float windowLength, float windowHeight) {
+        sf::Vector2f size(windowLength / 2.0f, windowHeight / 2.0f);
+        backGround.setSize(size);
+        backGround.setOrigin(size/2.0f);
+        backGround.setPosition(size);
+
+        closeMenuButton.setSize({ size.x / 30, size.x / 25 });
+        closeMenuButton.setOrigin({ closeMenuButton.getSize().x, 0 });
+        closeMenuButton.setPosition({ backGround.getPosition().x + size.x/2 - 5, backGround.getPosition().y - size.y/2 + 5});
+    }
+
+    //Array to hold all menu buttons
+    //std::vector<sf::RectangleShape>menuButtons;
+    //Vector to hold all menu text
+    //std::vector<sf::Text>menuText;
+    //sf::Text quitButton;
+    //sf::RectangleShape quitButtonBackGround;
+
+};
+
 long double G = 6.6743e-11;
 
 sf::Vector2u getDesktopResolution(int& horizontal, int& vertical) {
@@ -69,12 +127,6 @@ sf::Vector2<T> multiplyVectors(sf::Vector2<T> v1, sf::Vector2<T> v2) {
 sf::Vector2f multiplyVectorByDouble(sf::Vector2f v1, double scalar) {
 
     return sf::Vector2f(v1.x * scalar, v1.y * scalar);
-}
-
-//Function that divides a vector by a scalars
-sf::Vector2f divideVectorByDouble(sf::Vector2f v1, double scalar) {
-
-    return sf::Vector2f(v1.x / scalar, v1.y /scalar);
 }
 
 //Function to calculate planet velocity after a collision with another planet
@@ -137,16 +189,44 @@ int main()
 {
    
     sf::ContextSettings settings;
-    settings.antiAliasingLevel = 8;
+    settings.antiAliasingLevel = 4;
 
+    //--------------------SCALING STUFF------------------------------------
+    //-- Had to use this to get the windows scaling size (mine was set to 1.25 and was messing up the original
+    //-- function to retrieve screen size). This scaling can be applied where actual screensize is needed
+    auto activeWindow = GetActiveWindow();
+    HMONITOR monitor = MonitorFromWindow(activeWindow, MONITOR_DEFAULTTONEAREST);
+
+    // Get the logical width and height of the monitor
+    MONITORINFOEX monitorInfoEx;
+    monitorInfoEx.cbSize = sizeof(monitorInfoEx);
+    GetMonitorInfo(monitor, &monitorInfoEx);
+    auto cxLogical = monitorInfoEx.rcMonitor.right - monitorInfoEx.rcMonitor.left;
+    auto cyLogical = monitorInfoEx.rcMonitor.bottom - monitorInfoEx.rcMonitor.top;
+
+    // Get the physical width and height of the monitor
+    DEVMODE devMode;
+    devMode.dmSize = sizeof(devMode);
+    devMode.dmDriverExtra = 0;
+    EnumDisplaySettings(monitorInfoEx.szDevice, ENUM_CURRENT_SETTINGS, &devMode);
+    auto cxPhysical = devMode.dmPelsWidth;
+    auto cyPhysical = devMode.dmPelsHeight;
+
+    // Calculate the scaling factor
+    auto horizontalScale = ((double)cxPhysical / (double)cxLogical);
+    auto verticalScale = ((double)cyPhysical / (double)cyLogical);
+    //---------------------------------------------------------------------
     int horizontal = 0;
     int vertical = 0;
 
     sf::Vector2u screenResolution = getDesktopResolution(horizontal, vertical);
 
+
+
     //-------------------------------INITIALIZE--------------------------
-    sf::RenderWindow window(sf::VideoMode(screenResolution), "OpenGL", sf::Style::Default, sf::State::Fullscreen, settings);
+    sf::RenderWindow window(sf::VideoMode({ screenResolution.x, screenResolution.y}), "OpenGL", sf::Style::Default, sf::State::Fullscreen, settings);
     window.setVerticalSyncEnabled(true);
+    window.setKeyRepeatEnabled(false);
 
     std::vector<Planet> planets;
 
@@ -156,6 +236,8 @@ int main()
     planetTexture.loadFromFile("Assets/rusts.jpg");
     planetTexture.setSmooth(true);
 
+    Menu settingsMenu;
+
     //Main game loop
     while (window.isOpen())
     {
@@ -163,14 +245,100 @@ int main()
         //Constantly gets mouse position in the window
         sf::Vector2i localPosition = sf::Mouse::getPosition(window);
 
+        // --- DELTA TIME ---
+        float deltaTime;
+
         //While an event is happening
         while (const std::optional event = window.pollEvent())
         {
 
+            //=================================================//
+            //    .___  ___.  _______ .__   __.  __    __      //
+            //    |   \/   | |   ____||  \ |  | |  |  |  |     //
+            //    |  \  /  | |  |__   |   \|  | |  |  |  |     //
+            //    |  |\/|  | |   __|  |  . `  | |  |  |  |     //
+            //    |  |  |  | |  |____ |  |\   | |  `--'  |     //   
+            //    |__|  |__| |_______||__| \__|  \______/      //
+            //=================================================//  
+
+            //This is some TERRIBLE TERRIBLE logic, pls find way to clean later. PLEASE
+                                                                                                                                         
+            bool menu = false;
+
+            //If the event is the left mouse button
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+            {
+
+                bool pressed = true;
+                while (pressed)
+                {
+                    if (!(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)))
+                    {
+                        pressed = false;
+                    }
+                }
+
+                if (menu == false) {
+                    menu = true;
+                }
+            }
+
+            while (menu == true) {
+                deltaTime = 0;
+                std::cout << "Menu open" << std::endl;
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+                {
+                    //Logic so menu isnt spammed
+                    bool pressed = true;
+                    while (pressed)
+                    {
+                        if (!(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)))
+                        {
+                            pressed = false;
+                        }
+                    }
+
+                    if (menu == true) {
+                        menu = false;
+                    }
+                }
+
+                float scaledLength = screenResolution.x * horizontalScale;
+                float scaledHeight = screenResolution.y * horizontalScale;
+
+                settingsMenu.windowLength = scaledLength;
+                settingsMenu.windowHeight = scaledHeight;
+
+
+                settingsMenu.updateLayout(scaledLength, scaledHeight);
+
+                settingsMenu.draw(window);
+                window.display();
+
+                if (settingsMenu.closeMenuButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition()))) {
+                    menu = false;
+                }
+
+
+
+
+
+            }
+
+            //=======================================================================
+
+            deltaTime = clock.restart().asMilliseconds(); // seconds since last frame
+
             //If the event is clicking on the X or Escape then the window closes
-            if (event->is<sf::Event::Closed>() || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+            if (event->is<sf::Event::Closed>())
             {
                 window.close();
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
+                bool moving = true;
+
             }
 
             //If the event is the left mouse button
@@ -199,13 +367,10 @@ int main()
             }
         }
 
-        // --- DELTA TIME ---
-        float deltaTime = clock.restart().asMilliseconds(); // seconds since last frame
-
         //Planet accelerations stored and used later to update planet positions
         std::vector<sf::Vector2f> planetAccelerations(planets.size(), { 0.f, 0.f });
 
-
+        deltaTime = clock.restart().asMilliseconds(); // seconds since last frame
         // Math to calculate acceleration between planet. This is calculated from each planet to all other planets.
         for (std::size_t x = 0; x < planets.size(); ++x)
         {
@@ -228,7 +393,7 @@ int main()
                 planetAccelerations[x] += accelerationOnX;
                 planetAccelerations[y] += accelerationOnY;
 
-                std::cout << "Force: " << force << std::endl;  
+                //std::cout << "Force: " << force << std::endl;  
             }
         }
 
